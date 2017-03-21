075ce3cf4ae893e33c51f5d46f15c57813717bb0
shaikarshad <arshadshaik@my.unt.edu>
2017-03-21 13:39:54 -0500

    Initial map created

--- app/src/main/java/com/base/ours/eagleseyemainapp/StartActivity.java
+++ app/src/main/java/com/base/ours/eagleseyemainapp/StartActivity.java
@@ -1,13 +1,45 @@
 package com.base.ours.eagleseyemainapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.multidex.MultiDex;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.GoogleMap;
 
 public class StartActivity extends AppCompatActivity {
+    GoogleMap mMap;
+    private CoordinatorLayout coordinatorLayout;
+
+    @Override
+    protected void attachBaseContext(Context context) {
+        super.attachBaseContext(context);
+        MultiDex.install(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Button logInButton= (Button) findViewById(R.id.logInButton);
+        logInButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(StartActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
