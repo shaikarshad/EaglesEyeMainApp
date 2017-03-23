d981d3516654fa99feadc4827a84b1f79a1b450d
shaikarshad <arshadshaik@my.unt.edu>
2017-03-22 22:59:23 -0500

    Initial map created-2

--- app/src/main/java/com/base/ours/eagleseyemainapp/MapsActivity.java
+++ app/src/main/java/com/base/ours/eagleseyemainapp/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.base.ours.eagleseyemainapp;
 
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -39,8 +41,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Keeping discovery park as default location
-        LatLng discPark = new LatLng(33.2543416, -97.15247219999998);
-        mMap.addMarker(new MarkerOptions().position(discPark).title("Switch on Location setting for exact location"));
+        LatLng discPark = new LatLng(33.207513, -97.152730);
+        mMap.addMarker(new MarkerOptions().position(discPark).title("Discovery Park"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(discPark));
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
     }
 }
